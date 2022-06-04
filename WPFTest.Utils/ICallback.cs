12004eeced671f83ffaad67562341cddb5489cff
Eduardo Lucas Maia <eduardolucasmaia@hotmail.com>
2022-06-04 14:12:09 -0300

    Exclusão do projeto

--- WPFTest.Utils/ICallback.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WPFTest.Utils
-{
-    public delegate void IDelegateCallback(int result);
-    public delegate void IDelegateCallbackValueThreeObject(int result_0, int result_1, object result_2);
-    public delegate void IDelegateCallbackValueTwo(int result_0, int result_1);
-    public delegate void IDelegateCallbackValueTwoObject(int result_0, object result_1);
-}
